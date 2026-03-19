221c4654f5221406b8b5c055e102f5026f2c57c5
Lucas Maldonado <77515164+Lucasdm14@users.noreply.github.com>
2026-03-18 22:30:26 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
-  experimental: { serverActions: { bodySizeLimit: '10mb' } },
-}
-export default nextConfig
